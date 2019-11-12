c57f43a6a79dd46fcc7734592fac2e712a6de6fd
Lindong Zhao <lindong4067@sina.com>
2019-11-12 13:33:26 +0800

    cloud demo

---
+++ eureka-consumer-feign/src/main/java/com/example/eurekaconsumerfeign/EurekaConsumerFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaconsumerfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConsumerFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerFeignApplication.class, args);
+    }
+
+}
